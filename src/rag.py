9cb1fe7947ccf0471aa67edc9787833d700d48f6
Connor <connorgsmith2020@gmail.com>
2026-09-17 19:13:03 -0400

    Added qdrant , need tofix colelction befrore continuing to store embeddings

--- src/rag.py
+++ src/rag.py
@@ -1,6 +1,7 @@
 import ollama
 import os
-
+import qdrant_client as qdrant
+from qdrant_client.models import PointStruct, VectorParams, Distance
 def loadFiles(path)-> list: # type: ignore
     doc_list = []
     if not os.listdir(path):
@@ -28,10 +29,59 @@ def answerQuestion(question, doc_data):
     )
     return response["message"]["content"]
 
+
+def createEmbeddings(chunked_docs):
+    embeddings_list = []
+    for i in range(len(chunked_docs)):
+        chunk = chunked_docs[i]
+        embedding = ollama.embeddings(
+            model="nomic-embed-text",
+            prompt=chunk["chunk"]
+        )
+        _p = PointStruct(
+            id=i,
+            vector=embedding["embedding"],
+            payload={"source": chunk["source"], "chunk": chunk["chunk"]}
+        )
+        embeddings_list.append(_p)
+    return embeddings_list
+
 path = "../documents"
 document_list = loadFiles(path)
 
+
+def chunkDocs(doc_list):
+    chunkedDocs = []
+    for doc in doc_list:
+        source = doc["source"]
+        split_text = doc["text"].splitlines() # list of words split by \n
+        for i in range(len(split_text)):
+            with_name = {}
+            with_name["source"] = source
+            with_name["chunk"] = split_text[i]
+            chunkedDocs.append(with_name)
+
+    return chunkedDocs
+
+        
+
+
+
 print(len(document_list))
-question = input("Enter your question about the luzon straight")
-response = answerQuestion(question, document_list[0]["text"] if document_list else "")
-print(response)
+chunked = chunkDocs(document_list)
+embeddings_list = createEmbeddings(chunked)
+print(len(embeddings_list))
+
+qdrant_client = qdrant.QdrantClient(host="localhost", port=6333)
+qdrant_client.create_collection(
+    collection_name="document_collection",
+    vectors_config=VectorParams(size=len(embeddings_list[0].vector), distance=Distance.COSINE)
+)
+
+for point in embeddings_list:
+    qdrant_client.upsert(
+        collection_name="document_collection",
+        points=[point]
+    )
+
+print(qdrant_client.get_collection(collection_name="document_collection"))
